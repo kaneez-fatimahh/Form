080b81d8625aacfd5d43d01eef5d141e587e9798
kaneez <k.fatima.dev@gmail.com>
2024-01-19 05:47:30 -0800

    modifications

--- index.js
+++ index.js
@@ -6,64 +6,78 @@ let phone = document.getElementById("exampleInputPhone1");
 let email = document.getElementById("exampleInputEmail1");
 let password = document.getElementById("exampleInputPassword1");
 let container = document.getElementById("container");
-let container2 = document.getElementById("containerr");
-let cardboday = document.querySelectorAll(".card-body");
-
 
 // Empty All Feilds
-const emptyFeilds = ()=>{
-    name.value = "";
-    phone.value = "";
-    email.value = "";
-    password.value = "";
-    console.log("clear")
-}
-// Alert 
-const alert = (text , className)=>{
- let   parent = username.parentElement
-let alert = document.createElement("div");
- alert.className = `alert alert-${className}`
-alert.innerText =text;
-parent.insertBefore(alert,username);
-console.log("alert");
-setTimeout(() => {
+const emptyFeilds = () => {
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  password.value = "";
+  console.log("clear");
+};
+// Alert
+const alert = (text, className) => {
+  let parent = username.parentElement;
+  let alert = document.createElement("div");
+  alert.className = `alert alert-${className}`;
+  alert.innerText = text;
+  parent.insertBefore(alert, username);
+  console.log("alert");
+  setTimeout(() => {
     alert.remove();
-}, 3000);
-}
+  }, 3000);
+};
 
 //Display data on next page
-const display = ()=>{
- const getname = localStorage.getItem("name");
-  const getphone = localStorage.getItem("phone");
-  const getemail = localStorage.getItem("email");
-  const getpassword = localStorage.getItem("password");
-  let card =  document.createElement("div");
-  card.setAttribute("class","card" );
-  card.style.width = "18rem";
-  card.innerHTML += `  <h5 class="text-body-secondary px-2">Name:${getname}</h5>
-  <h5 class="card-subtitle mb-2 text-body-secondary px-2">Phone No:${getphone}</h5>
-  <h5 class="card-text text-body-secondary px-2">Email:${getemail}</h5>
-  <h5  class="card-text text-body-secondary px-2" >Password${getpassword}</h5>`
-container2.appendChild(card)
- }
+const display = () => {
+  let container2 = document.getElementById("container2");
+  let getdata = JSON.parse(localStorage.getItem("formData"));
+  console.log(getdata);
+  let obtain = getdata;
+  obtain.forEach((element) => {
+    let card = document.createElement("div");
+    card.classList.add("col-lg-4", "col-md-6", "col-sm-12", "mb-5", "mt-3");
+    card.classList.add("cards");
+    card.innerHTML += ` <div class="card m-auto" style="width: 16rem;">
+<img src="${element[4]}" class="card-img-top" alt="...">
+  <h6 class=" px-2 mt-2">Name : ${element[0]}</h6>
+  <h6 class="card-text mb-2 px-2 ">Phone : ${element[1]}</h6>
+  <h6 class="card-text  px-2">Email : ${element[2]}</h6>
+  <h6  class="card-text  px-2 mb-2" >Password : ${element[3]}</h6>
+</div>
+</div>  `;
+    container2.appendChild(card);
+  });
+};
 
-//Submit function
-let submitform = (e)=>{
-    if (name.value == "" || phone.value == "" ||email.value == ""||password.value == "") {
-        e.preventDefault();
-        alert("Please fill all the feilds!", "danger");
-    }else{
+let submitform = (e) => {
+  if (
+    name.value == "" ||
+    phone.value == "" ||
+    email.value == "" ||
+    password.value == ""
+  ) {
+    e.preventDefault();
+    alert("Please fill all the feilds!", "danger");
+  } else {
+    event.preventDefault();
+    let name = document.getElementById("exampleName1").value;
+    let phone = document.getElementById("exampleInputPhone1").value;
+    let email = document.getElementById("exampleInputEmail1").value;
+    let password = document.getElementById("exampleInputPassword1").value;
+    let img = profileImg.src;
+    const formData = [name, phone, email, password, img];
+    saveFormData(formData);
+    emptyFeilds();
+    window.location.href = "index2.html";
+    display();
+  }
+};
 
-         e.preventDefault();
-        localStorage.setItem("name",name.value);
-    localStorage.setItem("phone",phone.value);
-    localStorage.setItem("email",email.value);
-    localStorage.setItem("password",password.value);
-        emptyFeilds();
-        window.location.href = "index2.html"
-        display();
-        console.log("hy")
-    }
-     
+function saveFormData(formData) {
+  const storedFormData = JSON.parse(localStorage.getItem("formData")) || [];
+
+  storedFormData.push(formData);
+
+  localStorage.setItem("formData", JSON.stringify(storedFormData));
 }
-
